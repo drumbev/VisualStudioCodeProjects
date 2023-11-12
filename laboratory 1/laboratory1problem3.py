7a83da9bc17397901ffbd08c8101136f34602050
drumbev <drumbev@gmail.com>
2023-11-13 00:55:12 +0300

    комменты

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -1,5 +1,6 @@
 # y = x^0.5
-# graph = [[0 for i in range(10)] for i in range(12)]
+m, n = 22, 10
+graph = [[0 for i in range(m)] for i in range(n)]
 
 # # for i in graph:
     
@@ -13,9 +14,16 @@
 
 
 
-
-# for i in graph:
-#     print(' '.join(map(str, i)))
+#вывод графика с значениями х и у
+for i in range(1, len(graph)):
+    
+    print((n-i)*1000, ' . ','  '.join(map(str, graph[i])))
+print('     ', end = '')
+for j in range(1, 22):
+    print(j*3 + 33, end = ' ')
+print(end = '\n')
+    
+#оценка нужных значений
 a = []
 b = []
 for i in range(10):
@@ -25,7 +33,7 @@ for i in range(10):
         x = x//3*3
     else:
         x = x//3*3 + 3
-    print(y, y **0.5, x/3)
+    print(y, y **0.5, x, x/3)
     a.append(y**0.5)
 for i  in  range(len(a) -1):
     b.append(a[i]-a[i+1])
