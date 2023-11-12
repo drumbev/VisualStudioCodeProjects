33bff3c87ffd6c93a460349bf2b06c164942e09a
drumbev <drumbev@gmail.com>
2023-11-13 00:29:47 +0300

    pre

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -1,7 +1,7 @@
 # y = x^0.5
-graph = [[0 for i in range(10)] for i in range(12)]
+# graph = [[0 for i in range(10)] for i in range(12)]
 
-# for i in graph:
+# # for i in graph:
     
 
 
@@ -16,6 +16,18 @@ graph = [[0 for i in range(10)] for i in range(12)]
 
 # for i in graph:
 #     print(' '.join(map(str, i)))
-
+a = []
+b = []
 for i in range(10):
-    print((10-i)*10,((10 - i) * 10)**0.5)
+    y = (10-i)*1000
+    x = y **0.5
+    if abs(x - (x//3)*3)<=1.5:
+        x = x//3*3
+    else:
+        x = x//3*3 + 3
+    print(y, y **0.5, x/3)
+    a.append(y**0.5)
+for i  in  range(len(a) -1):
+    b.append(a[i]-a[i+1])
+print(b)
+    
