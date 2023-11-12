6af1c3d064dd04d9b3c5407984cc11b06289cbff
drumbev <drumbev@gmail.com>
2023-11-13 01:45:38 +0300

    исправленное

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -1,6 +1,6 @@
 # y = x^0.5
-m, n = 22, 10
-graph = [[0 for i in range(m)] for i in range(n)]
+m, n = 22, 9
+graph = [[0 for i in range(n)] for i in range(m)]
 
 # # for i in graph:
     
@@ -15,27 +15,23 @@ graph = [[0 for i in range(m)] for i in range(n)]
 
 
 #вывод графика с значениями х и у
-for i in range(1, len(graph)):
+for i in range(1, m):
     
-    print((n-i)*1000, ' . ','  '.join(map(str, graph[i])))
-print('     ', end = '')
-for j in range(1, 22):
-    print(j*3 + 33, end = ' ')
+    print((m-i)*3 + 33,' ', '   '.join(map(str, graph[i])))
+    
+print('     .' + '   .'*8)
+print('     ',end = '')
+for j in range(n):
+    print(f'{j+1}K ', end = ' ')
 print(end = '\n')
     
 #оценка нужных значений
 a = []
-b = []
-for i in range(10):
+for i in range(1, 10):
     y = (10-i)*1000
     x = y **0.5
     if abs(x - (x//3)*3)<=1.5:
         x = x//3*3
     else:
         x = x//3*3 + 3
-    print(y, y **0.5, x, x/3)
-    a.append(y**0.5)
-for i  in  range(len(a) -1):
-    b.append(a[i]-a[i+1])
-print(b)
-    
+    print(y, x)
