6453a78c8e20bd232fe37b63e7f01d2a4f9ea1f7
drumbev <drumbev@gmail.com>
2023-11-12 22:32:26 +0300

    1

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -1,4 +1,21 @@
 # y = x^0.5
-graph = [[0 for i in range(10)] for i in range(10)]
-for i in graph:
-    print(' '.join(map(str, i)))
+graph = [[0 for i in range(10)] for i in range(12)]
+
+# for i in graph:
+    
+
+
+
+
+
+
+
+
+
+
+
+# for i in graph:
+#     print(' '.join(map(str, i)))
+
+for i in range(10):
+    print((10-i)*10,((10 - i) * 10)**0.5)
