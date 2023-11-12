7c09fd2c8b456d18715488ac6a530e555d303b81
drumbev <drumbev@gmail.com>
2023-11-12 18:55:14 +0300

    wtf

--- laboratory 1/laboratory1work.py
+++ laboratory 1/laboratory1work.py
@@ -1,2 +1,9 @@
-print('This is laboratory work')
-print(555)
+RED = '\u001b[41m'
+BLUE = '\u001b[44m'
+WHITE = '\u001b[47m'
+END = '\u001b[0m'
+for i in range(6):
+    if i < 3:
+        print(f'{WHITE}{END}')
+    else:
+        print(f'{RED}{END}')
