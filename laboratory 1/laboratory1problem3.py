286f34bf15fa1dfbf75bf215b8b8fbb6daeb9df0
drumbev <drumbev@gmail.com>
2023-11-13 02:06:48 +0300

    ну по факту работает, но как-то фи

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -2,10 +2,29 @@
 m, n = 22, 9
 graph = [[0 for i in range(n)] for i in range(m)]
 
-# # for i in graph:
-    
+#оценка нужных значений
+a = []
+for i in range(1, 10):
+    y = (10-i)*1000
+    x = y **0.5
+    if abs(x - (x//3)*3)<=1.5:
+        x = int(x//3*3)
+    else:
+        x = int(x//3*3 + 3)
+    print(y, x)
 
 
+#заполнение значений
+for i in range(m):
+    for j in range(n):
+        x = (j+1)*1000
+        y = x **0.5
+        if abs(y - (y//3)*3)<=1.5:
+            y = int(y//3*3)
+        else:
+            y = int(y//3*3 + 3)  
+        if y == (m-i)*3 + 30:
+            graph[i][j] = '+'
 
 
 
@@ -15,23 +34,15 @@ graph = [[0 for i in range(n)] for i in range(m)]
 
 
 #вывод графика с значениями х и у
-for i in range(1, m):
-    
-    print((m-i)*3 + 33,' ', '   '.join(map(str, graph[i])))
-    
+for i in range(m):    
+    print((m-i)*3 + 30,' ', '   '.join(map(str, graph[i])))
+
+
+
+   
 print('     .' + '   .'*8)
 print('     ',end = '')
 for j in range(n):
     print(f'{j+1}K ', end = ' ')
 print(end = '\n')
     
-#оценка нужных значений
-a = []
-for i in range(1, 10):
-    y = (10-i)*1000
-    x = y **0.5
-    if abs(x - (x//3)*3)<=1.5:
-        x = x//3*3
-    else:
-        x = x//3*3 + 3
-    print(y, x)
