f21c5bb5479c06ab79f8739575cdc17526a3029e
drumbev <drumbev@gmail.com>
2023-11-13 15:21:41 +0300

    финальный вариант, так выглядит получше

--- laboratory 1/laboratory1problem3.py
+++ laboratory 1/laboratory1problem3.py
@@ -2,32 +2,38 @@
 m, n = 22, 9
 graph = [[0 for i in range(n)] for i in range(m)]
 
-#оценка нужных значений
-a = []
-for i in range(1, 10):
-    y = (10-i)*1000
-    x = y **0.5
-    if abs(x - (x//3)*3)<=1.5:
-        x = int(x//3*3)
-    else:
-        x = int(x//3*3 + 3)
-    print(y, x)
+#оценка нужных значений()
+# a = []
+# for i in range(1, 10):
+#     y = (10-i)*1000
+#     x = y **0.5
+#     if abs(x - (x//3)*3)<=1.5:
+#         x = int(x//3*3)
+#     else:
+#         x = int(x//3*3 + 3)
+#     print(y, x)
 
 
-#заполнение значений
+#заполнение значений(прошлый вариант)
+# for i in range(m):
+#     for j in range(n):
+#         x = (j+1)*1000
+#         y = x **0.5
+#         if abs(y - (y//3)*3)<=1.5:
+#             y = int(y//3*3)
+#         else:
+#             y = int(y//3*3 + 3)  
+#         if y == (m-i)*3 + 30:
+#             graph[i][j] = '+'
+
+
+# вроде так получше
 for i in range(m):
-    for j in range(n):
-        x = (j+1)*1000
-        y = x **0.5
-        if abs(y - (y//3)*3)<=1.5:
-            y = int(y//3*3)
-        else:
-            y = int(y//3*3 + 3)  
-        if y == (m-i)*3 + 30:
-            graph[i][j] = '+'
-
-
-
+    y = (m-i)*3+30
+    x = y**2
+    for j in range(1, 10):
+        if abs(x - j*1000)<=500:
+            graph[i][j-1] = '+'
 
 
 
@@ -37,9 +43,7 @@ for i in range(m):
 for i in range(m):    
     print((m-i)*3 + 30,' ', '   '.join(map(str, graph[i])))
 
-
-
-   
+  
 print('     .' + '   .'*8)
 print('     ',end = '')
 for j in range(n):
