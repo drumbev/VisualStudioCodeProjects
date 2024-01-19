26daf4880bef751b4346ae22f9179048e438e0a8
drumbev <drumbev@gmail.com>
2024-01-20 00:20:09 +0300

    mmanaging interval

--- laboratory 3/main.py
+++ laboratory 3/main.py
@@ -1,10 +1,22 @@
 from tkinter import *
-window = Tk()
-window.geometry('400x250')
-window.title("генерация ключа")
-lbl = Label(window, text = "МДА")
-lbl.grid(column=0, row=0)
+from random import randint
+characters = [i for i in range(49, 58)]+[i for i in range(65, 91)]
+def key():
+    return(f"{randint(0,10)}{randint(0,10)}{randint(0,10)}{randint(0,10)}{randint(0,10)}")
+def block():
+    
+#setting up the window
+# window = Tk()
+# window.geometry('576x360')
+# bg_img = PhotoImage(file='cursedminecraft.png')
+
+# lbl_bg = Label(window, image=bg_img)
+# lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
 
 
 
-window.mainloop()
+# window.mainloop()
+# from random import randint
+print(f"{key()}-{key()}")
+print([chr(i) for i in characters])
+# print(sum(characters)/len(characters))  71.2  71.2*5=356 so acceptable interval seems to be [300, 400]
